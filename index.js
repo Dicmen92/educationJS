e5d41a64a96619c0631af3f095c991381a7f617a
Dicmen92 <babich-d@mail.ru>
2022-11-30 14:43:04 +0600

    Выполнил первое задание

---
+++ index.js
@@ -0,0 +1,27 @@
+let money = prompt('Ваш бюджет на месяц?'),
+    time = prompt('Введите дату в формате YYYY-MM-DD'),
+    appData = {
+      budgetData: money,
+      timeData: time,
+      expenses: {},
+      optionalExpenses: {},
+      income: [],
+      savings: false
+    };
+
+    for (let i = 0; i < 2; i++) {
+      let expensesQuestion = prompt('Введите обязательную статью расходов в этом месяце'),
+          expensesAnswer = prompt('Во сколько обойдется?');
+         
+          if ((typeof(expensesQuestion) === 'string') && (typeof(expensesQuestion)) != null && (typeof(expensesAnswer)) != null && expensesQuestion != '' && expensesAnswer != '') {
+            appData.expenses[expensesQuestion] = expensesAnswer;           
+          } else {
+
+          }
+    }
+
+    let budgetDay = appData.budgetData / 30;
+    alert(budgetDay);
+       
+
+    console.log(appData);
