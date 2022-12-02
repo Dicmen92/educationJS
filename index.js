a6d3a2fc2ec5f3493a5261c7543cefe6f959f199
Dicmen92 <babich-d@mail.ru>
2022-12-02 17:27:22 +0600

    Обернул код в функции

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-let money = +prompt('Ваш бюджет на месяц?'),
-    time = prompt('Введите дату в формате YYYY-MM-DD'),
+let money, time;
+
+function start() {
+  money = +prompt('Ваш бюджет на месяц?');
+  
+  while(isNaN(money) || money == '' || money == null) {
+    money = +prompt('Ваш бюджет на месяц?');
+  }
+
+  time = prompt('Введите дату в формате YYYY-MM-DD');
+}
+
+start();
+
+
     appData = {
       budgetData: money,
       timeData: time,
@@ -9,19 +22,29 @@ let money = +prompt('Ваш бюджет на месяц?'),
       savings: false
     };
 
-    for (let i = 0; i < 2; i++) {
-      let expensesQuestion = prompt('Введите обязательную статью расходов в этом месяце'),
-          expensesAnswer = prompt('Во сколько обойдется?');
-         
-          if ((typeof(expensesQuestion) === 'string') && (typeof(expensesQuestion)) != null && (typeof(expensesAnswer)) != null && expensesQuestion != '' && expensesAnswer != '') {
-            appData.expenses[expensesQuestion] = expensesAnswer;           
-          } else {
-
-          }
+    function chooseExpenses() {
+      for (let i = 0; i < 2; i++) {
+        let expensesQuestion = prompt('Введите обязательную статью расходов в этом месяце'),
+            expensesAnswer = prompt('Во сколько обойдется?');
+           
+            if ((typeof(expensesQuestion) === 'string') && (typeof(expensesQuestion)) != null && (typeof(expensesAnswer)) != null && expensesQuestion != '' && expensesAnswer != '') {
+              appData.expenses[expensesQuestion] = expensesAnswer;           
+            } else {
+  
+            }
+      }
     }
 
-    let budgetDay = appData.budgetData / 30;
-    alert("Ваш ежедневный бюджет составляет: " + budgetDay);
+    chooseExpenses();
+    
+    function detectDayBudget() {
+      let budgetDay = (appData.budgetData / 30).toFixed();
+      alert("Ваш ежедневный бюджет составляет: " + budgetDay);
+    }
+
+    detectDayBudget();
+
+    
        
 
     console.log(appData);
